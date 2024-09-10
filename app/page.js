777aa2fc6b9ddc3727b0f879147d10d15d98906f
APS4087 <aungpyaesoe.bgs@Gmail.com>
2024-09-10 15:50:47 +0800

    updating landing page design

--- app/page.js
+++ app/page.js
@@ -1,34 +1,26 @@
-import Navbar from "@/components/Navbar";
+"use client";
+import Landing from "@/components/Landing";
+import { useEffect } from "react";
 
 export default function Home() {
+  useEffect(() => {
+    (async () => {
+      const LocomotiveScroll = (await import("locomotive-scroll")).default;
+      const locomotiveScroll = new LocomotiveScroll();
+    })();
+  }, []);
+
   return (
-    <div className="min-h-screen flex flex-col">
-      {/* Navbar */}
-      <Navbar />
-
-      {/* Main Content */}
-      <div className="flex-grow flex flex-col items-center relative px-4 sm:px-8 lg:px-16">
-        {/* Top Line */}
-        <div
-          className="absolute left-0 right-0 border-t-2 border-gray-400"
-          style={{ top: "1rem" }} // Position close to the top
-        ></div>
-
-        {/* Heading */}
-        <h1
-          className="text-6xl sm:text-8xl lg:text-10xl font-bold font-serif relative z-10"
-          style={{ marginTop: "2rem", marginBottom: "2rem" }}
-        >
-          AUNG PYAE SOE
-        </h1>
-
-        {/* Bottom Line 
-        <div
-          className="absolute left-0 right-0 border-b-2 border-gray-500"
-          style={{ top: "calc(25% + 3rem)" }} 
-        ></div>
-        */}
-      </div>
-    </div>
+    <main>
+      {/* <AnimatePresence mode='wait'>
+        {isLoading && <Preloader />}
+      </AnimatePresence>
+      <Landing />
+      <Description />
+      <Projects />
+      <SlidingImages />
+      <Contact /> */}
+      <Landing />
+    </main>
   );
 }
---
+++ components/Landing.js
@@ -0,0 +1,63 @@
+// landing component
+"use client";
+import Image from "next/image";
+import styles from "./landing.module.css";
+import { useRef, useEffect } from "react";
+import { animate } from "framer-motion";
+import gsap, { distribute } from "gsap";
+import ScrollTrigger from "gsap/ScrollTrigger";
+
+export default function Landing() {
+  const firstText = useRef(null);
+  const secondText = useRef(null);
+  const slider = useRef(null);
+  let xPercent = 0;
+  let direction = -1;
+
+  useEffect(() => {
+    gsap.set(secondText.current, {
+      left: secondText.current.getBoundingClientRect().width,
+    });
+    requestAnimationFrame(animate);
+  }, []);
+
+  // smooth scroll
+  useEffect(() => {
+    gsap.registerPlugin(ScrollTrigger);
+    gsap.to(slider.current, {
+      scrollTrigger: {
+        trigger: document.documentElement,
+        scrub: 0.5,
+        start: 0,
+        end: window.innerHeight,
+        onUpdate: (e) => (direction = e.direction * -1),
+      },
+      x: "-500px",
+    });
+    requestAnimationFrame(animate);
+  });
+
+  const animate = () => {
+    if (xPercent < -100) {
+      xPercent = 0;
+    } else if (xPercent > 0) {
+      xPercent = -100;
+    }
+    gsap.set(firstText.current, { xPercent: xPercent });
+    gsap.set(secondText.current, { xPercent: xPercent });
+    requestAnimationFrame(animate);
+    xPercent += 0.05 * direction;
+  };
+
+  return (
+    <main className={styles.main}>
+      <Image src="/images/bg.jpg" fill={true} alt="background" />
+      <div className={styles.sliderContainer}>
+        <div ref={slider} className={styles.slider}>
+          <p ref={firstText}>Freelance Developer -</p>
+          <p ref={secondText}>Freelance Developer -</p>
+        </div>
+      </div>
+    </main>
+  );
+}
